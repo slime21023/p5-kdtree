c675f751fbd4004ed7b5bdac4b61a337050a93dc
slime21023 <slime21023@gmail.com>
2023-03-02 15:25:37 +0800

    fix kdtree constructor

--- src/sketch.js
+++ src/sketch.js
@@ -1,22 +1,41 @@
 
 class KDtree {
 
-    static computeEdges(dataPoints, axis, {minX, maxX, minY, maxY}, leafsize=10) {
+    static computeEdges(tree, { minX, maxX, minY, maxY }) {
+        function computeBounds(edge) {
+            const { startX, startY, endX, endY } = edge
+            let leftBound = {
+                minX,
+                maxX: endX,
+                minY,
+                maxY: endY
+            }
+
+            let rightBound = {
+                minX: startX,
+                maxX,
+                minY: startY,
+                maxY
+            }
+
+            return { leftBound, rightBound }
+        }
+
         let edges = []
-        if (dataPoints.length < leafsize) {
+        if (tree.median == null) {
             return edges
         }
-        
-        const { median, left, right } = KDtree.partition(dataPoints, axis)
-        if (axis==0) {
-            edges = [{ 
+
+        const { median, left, right, axis } = tree
+        if (axis == 0) {
+            edges = [{
                 startX: median.x,
                 startY: minY,
                 endX: median.x,
                 endY: maxY
             }]
         } else {
-            edges = [{ 
+            edges = [{
                 startX: minX,
                 startY: median.y,
                 endX: maxX,
@@ -24,33 +43,50 @@ class KDtree {
             }]
         }
 
-        // compute subtree edges
-        if (left.length >= leafsize) {
-            const  leftBound = { 
-                minX, 
-                maxX: (axis==0) ? median.x : maxX,
-                minY,
-                maxY: (axis==0) ? maxY: median.y
-            }
-            edges = edges.concat(KDtree.computeEdges(left, (axis +1)%2, leftBound, leafsize))
-        }
-
-        if (right.length >= leafsize) {
-            const rightBound = {
-                minX: (axis == 0) ? median.x : minX,
-                maxX,
-                minY: (axis == 0) ? minY : median.y,
-                maxY 
-            }
-            edges = edges.concat(KDtree.computeEdges(right, (axis +1)%2, rightBound, leafsize))
-        }
+        const { leftBound, rightBound } = computeBounds(edges[0])
+        edges = edges.concat(
+            KDtree.computeEdges(left, leftBound),
+            KDtree.computeEdges(right, rightBound),
+        )
 
         return edges
     }
 
+    static getTotalDepth(tree) {
+        if (tree.median == null) {
+            return tree.depth
+        }
+
+        let leftDepth = KDtree.getTotalDepth(tree.left)
+        let rightDepth = KDtree.getTotalDepth(tree.right)
+
+        let depth = (leftDepth >= rightDepth) ? leftDepth : rightDepth
+        return depth
+    }
+
+    static filteByDepth(tree, depth) {
+        if (tree.depth == depth && tree.median == null) {
+            return []
+        }
+
+        if (tree.depth == depth && tree.median != null) {
+            return [tree.median]
+        }
+
+        let result = []
+        if (tree.depth < depth && tree.median != null) {
+            result = result.concat(
+                KDtree.filteByDepth(tree.left, depth),
+                KDtree.filteByDepth(tree.right, depth)
+            )
+        }
+
+        return result
+    }
+
     static partition(datapoints, axis) {
         const comparer = (a, b) => {
-            if(axis == 0) {
+            if (axis == 0) {
                 return a.x - b.x;
             }
             return a.y - b.y;
@@ -66,30 +102,14 @@ class KDtree {
         }
     }
 
-    static getTotalDepth(tree) {
-        if(tree.median == null) {
-            return 0
-        }
-
-        let leftDepth = KDtree.getTotalDepth(tree.left)
-        let rightDepth = KDtree.getTotalDepth(tree.right)
-
-        let depth = (leftDepth >= rightDepth) ? leftDepth : rightDepth
-        return depth + 1  
-    }
-
-    static filteByDepth(depth) {
-        
-    }
-
-    constructor(datapoints,  leafsize=10, depth=0) {
+    constructor(datapoints, leafsize = 10, depth = 0) {
         this.axis = depth % 2
         this.depth = depth
         if (datapoints.length >= leafsize) {
-            const { median, left, right } = KDtree.partition(datapoints, (depth+1) %2)
+            const { median, left, right } = KDtree.partition(datapoints, this.axis)
             this.median = median
-            this.left = new KDtree(left, leafsize, depth+1)
-            this.right = new KDtree(right, leafsize, depth+1)
+            this.left = new KDtree(left, leafsize, depth + 1)
+            this.right = new KDtree(right, leafsize, depth + 1)
         } else {
             this.datapoints = datapoints
             this.left = null
@@ -102,43 +122,88 @@ class KDtree {
 
 
 let dataPoints = [];
-let pointNum = 200; 
+let pointNum = 200;
 let edges = []
 let tree;
 let totalDepth = 0;
+let leafsize = 4
 
-function setup() {
-    let leafsize = 4
-    createCanvas(400, 400)
-    for(i = 0; i < pointNum; i++) {
-        dataPoints[i] = { x: random(10, 390), y: random(10, 390) }
+const fullDisplay = (sketch) => {
+
+    sketch.setup = () => {
+        sketch.createCanvas(400, 400)
+        for (i = 0; i < pointNum; i++) {
+            let x = Math.floor(sketch.random(10, 390))
+            let y = Math.floor(sketch.random(10, 390))
+
+            dataPoints[i] = { x, y }
+        }
+
+        tree = new KDtree(dataPoints, leafsize)
+
+        edges = KDtree.computeEdges(tree, {
+            minX: 0, maxX: 400, minY: 0, maxY: 400
+        })
+
+        totalDepth = KDtree.getTotalDepth(tree)
+        console.log(totalDepth)
     }
 
-    edges = KDtree.computeEdges(dataPoints, 0, {
-        minX: 0, maxX: 400, minY: 0, maxY: 400
-    }, leafsize)
+    sketch.draw = () => {
+        sketch.background(220);
+        sketch.stroke('purple');
+        sketch.strokeWeight(4);
 
-    tree = new KDtree(dataPoints, leafsize)
-    totalDepth = KDtree.getTotalDepth(tree)
-    console.log(totalDepth)
+        for (i = 0; i < pointNum; i++) {
+            sketch.point(dataPoints[i].x, dataPoints[i].y);
+        }
+
+        sketch.strokeWeight(1);
+        sketch.stroke('green');
+        for (edge of edges) {
+            const { startX, startY, endX, endY } = edge
+            sketch.line(startX, startY, endX, endY)
+        }
+    }
 }
 
 
-function draw() {
-    background(220);
-    stroke('purple');
-    strokeWeight(5);
+// Partial Display Variable
+let filtedPoints = []
+let partialDepth = 4
+let partialTree
+let partialEdges = []
 
-    for(i = 0; i < pointNum; i++) {
-        point(dataPoints[i].x, dataPoints[i].y);
+const partialDisplay = (sketch) => {
+    sketch.setup = () => {
+        sketch.createCanvas(400, 400)
+        filtedPoints = KDtree.filteByDepth(tree, partialDepth)
+        partialTree = new KDtree(filtedPoints, leafsize)
+        partialEdges = KDtree.computeEdges(partialTree, {
+            minX: 0, maxX: 400, minY: 0, maxY: 400
+        })
+        console.log(filtedPoints)
+        console.log(partialTree)
     }
 
+    sketch.draw = () => {
+        sketch.background(220);
+        sketch.stroke('purple');
+        sketch.strokeWeight(5);
 
-    stroke('green');
-    for(edge of edges) {
-        const { startX, startY, endX, endY } = edge
-        line(startX, startY, endX, endY)
+        for (const point of filtedPoints) {
+            sketch.point(point.x, point.y);
+        }
+
+        sketch.strokeWeight(1);
+        sketch.stroke('red');
+        for (edge of partialEdges) {
+            const { startX, startY, endX, endY } = edge
+            sketch.line(startX, startY, endX, endY)
+        }
     }
-    // point(30, 20);
-    // point(85, 20);
 }
+
+
+const fullP5 = new p5(fullDisplay, 'fullDisplay')
+const partialP5 = new p5(partialDisplay, 'partialDisplay')
